c7ba09a6df06d26cb9813887c99f0dfd9560ca7a
Aron <danberl4k3@gmail.com>
2026-09-10 00:32:17 -0500

    Correct capacity regression test hour

--- tests/bookings.test.ts
+++ tests/bookings.test.ts
@@ -82,7 +82,7 @@ describe('reglas de reserva', () => {
   });
 
   it('respeta la capacidad configurada por hora', () => {
-    db.prepare('UPDATE capacity_slots SET max_slots = 1 WHERE hour = 8').run();
+    db.prepare('UPDATE capacity_slots SET max_slots = 1 WHERE hour = 10').run();
     createBooking(db, validInput(), { now: mondayAt(8) });
     expect(() =>
       createBooking(db, { ...validInput(), plate: 'XYZ-987' }, { now: mondayAt(8) }),
